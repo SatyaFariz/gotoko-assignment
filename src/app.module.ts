e52bcacf1103f581a329033560c80ffb618d1fe5
Satya Fariz <satyafariznur@gmail.com>
2023-01-07 04:36:44 +0700

    test orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({ 
+      type: 'mysql',
+      host: 'db',
+      username: 'admin',
+      password: 'password',
+      database: 'app',
+      entities: ['dist/**/*.entity.js'],
+      synchronize: true
+    }), 
+    UserModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
