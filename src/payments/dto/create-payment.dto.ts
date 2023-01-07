3b71a978cd1a08f42704647c7c51a69c66e4473e
Satya Fariz <satyafariznur@gmail.com>
2023-01-08 06:23:48 +0700

    add validation dto

--- src/payments/dto/create-payment.dto.ts
+++ src/payments/dto/create-payment.dto.ts
@@ -1,1 +1,21 @@
-export class CreatePaymentDto {}
+import { IsNotEmpty, IsString, IsEnum, IsUrl, ValidateIf } from 'class-validator'
+
+export enum PaymentType {
+  'CASH' = 'CASH',
+  'E-WALLET' = 'E-WALLET',
+  'EDC' = 'EDC'
+}
+
+export class CreatePaymentDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string
+
+  @IsNotEmpty()
+  @IsEnum(PaymentType)
+  type: PaymentType
+
+  @IsUrl()
+  @ValidateIf((_, value) => value !== null)
+  logo: string | null
+}
