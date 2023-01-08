c2ca93907acd9c6efc1eca2c14cb876d3208520c
Satya Fariz <satyafariznur@gmail.com>
2023-01-08 11:23:30 +0700

    add success interceptors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SuccessInterceptor } from './interceptors/success'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+  app.useGlobalInterceptors(new SuccessInterceptor());
   await app.listen(3000);
 }
 bootstrap();
