9d8b3b6c60eef573c72c21348ad633022d8f560a
Satya Fariz <satyafariznur@gmail.com>
2023-01-10 03:34:43 +0700

    removed unused vars

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,7 +1,6 @@
-import { Controller, Get, Post, Body, Put, Param, Delete, ParseIntPipe, Query, ParseArrayPipe } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, ParseIntPipe, Query, ParseArrayPipe } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { CreateOrderDto } from './dto/create-order.dto';
-import { UpdateOrderDto } from './dto/update-order.dto';
 import { FindOrdersQueryParamsDto } from './dto/find-orders-query-params.dto'
 import { GetSubtotalDto } from './dto/get-subtotal.dto'
 import generateInvalidFieldsErrorObject from '../helpers/generateInvalidFieldsErrorObject'
