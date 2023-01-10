b7b4746f5143ff6f2dcd78fdfe75308da51f58cd
Satya Fariz <satyafariznur@gmail.com>
2023-01-10 21:22:43 +0700

    add env vars

--- db/data-source.ts
+++ db/data-source.ts
@@ -2,13 +2,13 @@ import { DataSourceOptions, DataSource } from "typeorm";
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'mysql',
-  host: 'db',//'localhost',
-  username: 'admin',
-  password: 'password',
-  database: 'app',
+  host: process.env.MYSQL_HOST,//'db',//'localhost',
+  username: process.env.MYSQL_USER,//'admin',
+  password: process.env.MYSQL_PASSWORD,//'password',
+  database: process.env.MYSQL_DBNAME,//'app',
   entities: ['dist/**/*.entity.js'],
   migrations: ['dist/db/migrations/*.js']
 }
 
-const dataSource = new DataSource({ ...dataSourceOptions, host: 'localhost' })
+const dataSource = new DataSource({ ...dataSourceOptions })
 export default dataSource
