c5076826a3a4723f9bc608114d79aa2520359a98
Satya Fariz <satyafariznur@gmail.com>
2023-01-12 03:29:35 +0700

    testing

--- run.sh
+++ run.sh
@@ -1,1 +1,7 @@
-redis-server --daemonize yes && npm run migration:run && npm run build && npm run start
+# redis-server --daemonize yes && npm run migration:run && npm run build && npm run start
+
+redis-server & /entrypoint.sh mysqld & (sleep 90; npm run build; npm run start) &
+
+# docker run --rm testing:latest1 -p 3030:3030
+
+# docker run --rm -ti -p 3030:3030 satyafariz/gotoko-test:latest /bin/bash
